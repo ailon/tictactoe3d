b1c7cacfdbe00db7f3ce1f0dea05e8cdd17ab786
Alan Mendelevich <ailon@ailon.org>
2010-01-09 17:17:41 +0200

    added highlighting animation for new scoring lines

--- TicTacToe3D/Board.cs
+++ TicTacToe3D/Board.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace TicTacToe3D
 {
@@ -30,7 +31,17 @@ namespace TicTacToe3D
                 UIElement logo = (UIElement)GetTemplateChild("LOGO");
                 logo.Visibility = Visibility.Collapsed;
             }
+
+            for (int c = 0; c < 3; c++)
+            {
+                for (int r = 0; r < 3; r++)
+                {
+                    _highlighters[c, r] = (Rectangle)GetTemplateChild("HL_" + c.ToString() + r.ToString());
+                }
+            }
         }
+
+        private Rectangle[,] _highlighters = new Rectangle[3, 3];
 
         protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
         {
@@ -42,6 +53,22 @@ namespace TicTacToe3D
         public void Clear()
         {
             BC.Children.Clear();
+        }
+
+        public void HighlightCell(int column, int row, Color color)
+        {
+            _highlighters[column, row].Fill = new SolidColorBrush(color);
+            Storyboard sb = new Storyboard();
+            DoubleAnimation ca = new DoubleAnimation();
+            ca.From = 0;
+            ca.To = 1;
+            ca.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            ca.AutoReverse = true;
+            ca.RepeatBehavior = new RepeatBehavior(3);
+            sb.Children.Add(ca);
+            Storyboard.SetTarget(sb, _highlighters[column, row].Fill);
+            Storyboard.SetTargetProperty(sb, new PropertyPath(SolidColorBrush.OpacityProperty));
+            sb.Begin();
         }
 	}
 }
---
+++ TicTacToe3D/Cell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TicTacToe3D
+{
+    public class Cell
+    {
+        public Cell(int plane, int column, int row)
+        {
+            Plane = plane;
+            Column = column;
+            Row = row;
+        }
+
+        public int Plane { get; set; }
+        public int Column { get; set; }
+        public int Row { get; set; }
+
+        public static bool operator ==(Cell cell1, Cell cell2)
+        {
+            return cell1.Equals(cell2);
+        }
+
+        public static bool operator !=(Cell cell1, Cell cell2)
+        {
+            return !cell1.Equals(cell2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Cell cell = (Cell)obj;
+            return this.Plane == cell.Plane && this.Column == cell.Column && this.Row == cell.Row;
+        }
+
+        public override int GetHashCode()
+        {
+            return Plane * 100 + Column * 10 + Row;
+        }
+    }
+}
